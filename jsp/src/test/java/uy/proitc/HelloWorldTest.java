a73bffd49a5ee921c86688ad85a17a9692b7e2a3
Juan <earth001@gmail.com>
2020-10-07 10:09:37 -0300

    Added test with arquillian graphene

--- jsp/src/test/java/uy/proitc/HelloWorldTest.java
+++ jsp/src/test/java/uy/proitc/HelloWorldTest.java
@@ -9,18 +9,24 @@ import java.io.File;
 import java.net.URL;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
 
 @RunWith(Arquillian.class)
 public class HelloWorldTest {
 
   @ArquillianResource
   private URL baseUrl;
-
+  @Drone
+  private WebDriver browser;
+  @Page
+  private IndexPageObject index;
 
   @RunAsClient
   @Test
@@ -29,6 +35,13 @@ public class HelloWorldTest {
       HtmlPage page = webClient.getPage(baseUrl);
       assertThat(page.asXml()).contains("Hello World!");
     }
+  }
+
+  @RunAsClient
+  @Test
+  public void whenGetIndexPageWithDrone_thenGetHelloWorld() throws Exception {
+    browser.get(baseUrl.toExternalForm() + "/index.jsp");
+    assertThat(index.getH1().getText()).contains("Hello World!");
   }
 
   @Deployment
---
+++ jsp/src/test/java/uy/proitc/IndexPageObject.java
@@ -0,0 +1,16 @@
+package uy.proitc;
+
+import org.jboss.arquillian.graphene.page.Location;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+@Location("/index.jsp")
+public class IndexPageObject {
+
+  @FindBy(xpath = "/html/body/h1")
+  private WebElement h1;
+
+  public WebElement getH1() {
+    return h1;
+  }
+}
